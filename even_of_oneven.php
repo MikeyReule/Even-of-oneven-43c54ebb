96716791fe18dad28e365b03276a0de1ccfa50d0
Mike <2114794@talnet.nl>
2019-10-30 14:00:59 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,15 @@
+<?php
+
+$num = readline("Vul een getal in: ");
+echo PHP_EOL;
+
+if ($num % 2 == 0) {
+	echo "Dit is een even getal";
+	echo PHP_EOL;
+} else {
+	echo "Dit is een oneven getal";
+	echo PHP_EOL;
+}
+
+
+?>
